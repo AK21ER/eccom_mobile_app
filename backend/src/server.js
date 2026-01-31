c9fde32fdf9a19d30104102df852ee781c0f8762
AK21ER <itsmereka21@gmail.com>
2026-01-31 23:13:48 +0300

    deployment of config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from "express";
+import path from "path";
+import { PORT, NODE_ENV } from "./config/env.js";
 
 const app = express();
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+if (NODE_ENV === "production") {
+    const adminDistPath = path.join(import.meta.dirname, "../../admin/dist");
+    app.use(express.static(adminDistPath));
+
+    app.get("{*splat}", (req, res) => {
+        res.sendFile(path.join(adminDistPath, "index.html"));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
