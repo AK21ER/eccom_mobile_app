274645e3f85767cb33aa74817a984db92705dafb
AK21ER <itsmereka21@gmail.com>
2026-02-09 22:50:32 +0300

    use socialauth done

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,16 +2,17 @@ import { HeaderShownContext } from "@react-navigation/elements";
 import { Stack } from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import {QueryClientProvider,QueryClient} from "@tanstack/react-query"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 import "../global.css"
 
 const queryClient = new QueryClient()
 
 export default function RootLayout() {
-  return 
-<ClerkProvider tokenCache={tokenCache}>
-<QueryClientProvider client={queryClient}>
-    <Stack  screenOptions={{headerShown : false}}/>
-    </QueryClientProvider>
-    </ClerkProvider>  ;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </QueryClientProvider>
+    </ClerkProvider>
+  );
 }
