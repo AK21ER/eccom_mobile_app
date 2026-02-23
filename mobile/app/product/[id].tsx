1bcc0dbe52615ebe4368eefba1f4486d115c0441
AK21ER <itsmereka21@gmail.com>
2026-02-23 22:19:14 +0300

    some imports

--- mobile/app/product/[id].tsx
+++ mobile/app/product/[id].tsx
@@ -1,6 +1,22 @@
+import SafeScreen from "@/components/SafeScreen";
+import useCart from "@/hooks/useCart";
+import { useProduct } from "@/hooks/useProduct";
+import useWishlist from "@/hooks/useWishlist";
+import { Ionicons } from "@expo/vector-icons";
+import { Image } from "expo-image";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  Alert,
+  ActivityIndicator,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 
-
-
+const { width } = Dimensions.get("window");
 
 const ProductDetailScreen = () => {
 
