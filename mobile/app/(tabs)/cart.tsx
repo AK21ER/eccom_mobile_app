d0caf1089f2faa8ff32eae25918d6e52ce6f10b8
AK21ER <itsmereka21@gmail.com>
2026-02-24 19:08:07 +0300

    loding ui in cart tab

--- mobile/app/(tabs)/cart.tsx
+++ mobile/app/(tabs)/cart.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import SafeScreen from '@/components/SafeScreen'
+import { ActivityIndicator } from 'react-native'
 
 const CartScreen = () => {
   return (
@@ -13,3 +14,11 @@ const CartScreen = () => {
 }
 
 export default CartScreen
+function LoadingUI() {
+  return (
+    <View className="flex-1 bg-background items-center justify-center">
+      <ActivityIndicator size="large" color="#00D9FF" />
+      <Text className="text-text-secondary mt-4">Loading cart...</Text>
+    </View>
+  );
+}
