042aa559a93e1657c56dc8e53fd00337824ab303
AK21ER <itsmereka21@gmail.com>
2026-02-26 16:42:56 +0300

    address modal imports

---
+++ mobile/components/AddressSelectionModal.tsx
@@ -0,0 +1,17 @@
+import { useAddresses } from "@/hooks/useAddressess";
+import { Address } from "@/types";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import { View, Text, Modal, TouchableOpacity, ScrollView, ActivityIndicator } from "react-native";
+
+
+
+const AddressSelectionModal = () => {
+ 
+
+  return (
+   
+  );
+};
+
+export default AddressSelectionModal;
