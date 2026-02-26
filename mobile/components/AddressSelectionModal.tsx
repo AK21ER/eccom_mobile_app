025816c3cf2dc0ade35b3efb24825905d95ed1fb
AK21ER <itsmereka21@gmail.com>
2026-02-26 16:43:31 +0300

    interface

--- mobile/components/AddressSelectionModal.tsx
+++ mobile/components/AddressSelectionModal.tsx
@@ -4,7 +4,12 @@ import { Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
 import { View, Text, Modal, TouchableOpacity, ScrollView, ActivityIndicator } from "react-native";
 
-
+interface AddressSelectionModalProps {
+  visible: boolean;
+  onClose: () => void;
+  onProceed: (address: Address) => void;
+  isProcessing: boolean;
+}
 
 const AddressSelectionModal = () => {
  
