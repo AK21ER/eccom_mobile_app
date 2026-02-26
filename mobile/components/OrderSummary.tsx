bdc9214bf5282bbf78a2d4b350b1eb4b245b69ba
AK21ER <itsmereka21@gmail.com>
2026-02-26 16:51:11 +0300

    ordersummary component interface

--- mobile/components/OrderSummary.tsx
+++ mobile/components/OrderSummary.tsx
@@ -1,6 +1,11 @@
 import { View, Text } from "react-native";
 
-
+interface OrderSummaryProps {
+  subtotal: number;
+  shipping: number;
+  tax: number;
+  total: number;
+}
 
 export default function OrderSummary() {
   return (
